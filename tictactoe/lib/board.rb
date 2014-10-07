41b3d49c04c30ed92912019f0223d27b45c8c6ec
shannon <shannonjc143@gmail.com>
2014-10-07 15:19:13 -0500

    still not working

--- tictactoe/lib/board.rb
+++ tictactoe/lib/board.rb
@@ -1,8 +1,8 @@
 
 class Board
   attr_accessor :arr
-  def initialize(arr = [1, 2, 3, 4, 5, 6, 7, 8, 9])
-    @arr = arr
+  def initialize
+    @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
   end
   
   def print_board 
@@ -21,14 +21,63 @@ class Board
 
 
   def add_x_board(choice)
-    @arr[(choice - 1)] = 'X'
-    return @arr    
+    @arr[(choice - 1)] = 'X'    
   end
  
   def add_o_board(choice)
     @arr[(choice - 1)] = 'O'
-    return @arr
+
   end
+
+  def get_rows
+    @board.each_slice(3).to_a
+  end
+
+def check_array_equal(arr)
+    arr.all? { |item| item == arr[0] }
+  end
+
+  def check_for_win
+    check_for_row_win || check_for_column_win || check_for_diag_win
+  end
+  
+  def check_for_row_win   
+    @board.get_rows.any? { |row| check_array_equal(row) }
+  end
+   
+  def get_columns
+    rows = @board.get_rows
+    columns = Array.new
+    columns << rows.map { |i| i[rows.length-1] } 
+    columns << rows.map { |i| i[rows.length-2] }
+    columns << rows.map { |i| i[rows.length-3] }
+  end
+
+  def get_diags(diags)
+    diag_arr = Array.new
+    rows = @board.get_rows
+    make_diag_arr = 
+    rows.each_with_index { |row, index| diag_arr << row[index] }
+    rows.reverse.each_with_index { |row, index| diag_arr << row[index] }
+    diags = diag_arr.each_slice(3).to_a 
+  end
+
+  def check_for_column_win
+    get_columns.any? { |column| check_array_equal(column) }
+  end
+
+  def check_for_diag_win
+    get_diags.any? { |diag| check_array_equal(diag) }
+  end  
+
+  def check_for_tie(board)
+    if check_for_win(board) == false and board.all? { |place| place.class == String }
+      true
+    else
+      false
+    end
+  end
+ 
 
 
   end
--- tictactoe/lib/game.rb
+++ tictactoe/lib/game.rb
@@ -18,7 +18,7 @@ class Game
   end
 
   def initialize
-
+    
     @board = Board.new
   end
 
@@ -46,58 +46,12 @@ class Game
     @current_player = current_player
   end
   
-  def get_rows(board)
-    rows = board.each_slice(3).to_a
-  end
 
-  def check_array_equal(arr)
-    arr.all? { |item| item == arr[0] }
-  end
 
-  def check_for_win(board)
-    check_for_row_win(board) || check_for_column_win(board) || check_for_diag_win(board)
-  end
   
-  def check_for_row_win(board)   
-    get_rows(board).any? { |row| check_array_equal(row) }
-  end
-   
-  def get_columns(board)
-    rows = get_rows(board)
-    columns = Array.new
-    columns << rows.map { |i| i[rows.length-1] } 
-    columns << rows.map { |i| i[rows.length-2] }
-    columns << rows.map { |i| i[rows.length-3] }
-  end
-
-  def get_diags(board)
-    diag_arr = Array.new
-    rows = get_rows(board)
-    make_diag_arr = 
-    rows.each_with_index { |row, index| diag_arr << row[index] }
-    rows.reverse.each_with_index { |row, index| diag_arr << row[index] }
-    diags = diag_arr.each_slice(3).to_a 
-  end
-
-  def check_for_column_win(board)
-    get_columns(board).any? { |column| check_array_equal(column) }
-  end
-
-  def check_for_diag_win(board)
-    get_diags(board).any? { |diag| check_array_equal(diag) }
-  end  
-
-  def check_for_tie(board)
-    if check_for_win(board) == false and board.all? { |place| place.class == String }
-      true
-    else
-      false
-    end
-  end
- 
   def decide_winner(board)
     if  check_for_row_win(board) == true
-      winner = get_rows(board).select { |item| check_array_equal(item) == true }[0][0]
+      winner = @board.get_rows.select { |item| check_array_equal(item) == true }[0][0]
       winner
       return @current_player
     elsif check_for_column_win(board) == true
@@ -123,7 +77,8 @@ class Game
  def play  
     get_names
     decide_first_player
-    while check_for_win(@board.arr) == false && check_for_tie(@board.arr) == false
+    @board = Board.new
+    while @board.check_for_win == false && check_for_tie(@board) == false
       choice = get_choice
       if @current_player == @xplayer
         @board.add_x_board(choice)
@@ -132,15 +87,16 @@ class Game
       end
       switch_player
     end
-    if check_for_win(@board.arr.to_a) == true
+    if check_for_win(@board) == true
       token = decide_winner(@board)
     "#{token}, you won! Play again?"
     end
-    if check_for_tie(@board.arr) == true
+    if check_for_tie(@board) == true
       puts "This game is a tie. Play again?"
     end
     response = $stdin.gets.chomp.downcase
     if response.start_with?('y')
+
       play
     else
       puts "Goodbye"
